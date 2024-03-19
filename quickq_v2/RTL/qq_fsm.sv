2c445a9078348cb199ec4a939ccaed93ec203d0f
John Nestor <nestorj@lafayette.edu>
2024-03-19 08:23:00 -0400

    latch inference on next in DEQ1 state was the problem

--- quickq_v2/RTL/qq_fsm.sv
+++ quickq_v2/RTL/qq_fsm.sv
@@ -22,9 +22,7 @@ module qq_fsm #(
     enq_i,
     deq_i,
     t_gt_ram_out,
-    full,
     input logic [DW-1:0] wr_addr,
-    rd_addr,
     input logic [W-1:0] ram_out,
     output logic sel_t,
     ld_t,
@@ -79,6 +77,7 @@ module qq_fsm #(
         clr_empty = 0;
         incr_ctl = 2'b00;
         sel_b = 2'b00;
+        next = IDLE;
 
         case (state)
             INIT1: begin
@@ -152,8 +151,7 @@ module qq_fsm #(
                 if (ram_out == MAX_KEY) begin
                     clr_full   = 1;
                     clr_wraddr = 1;
-                    if (wr_addr == 0 && rd_addr == 2 && !full) set_empty = 1;  // only cycle in this case, rd_addr always 2 initially
-                    if (rd_addr == D-2 && full) we = 0;
+                    if (wr_addr == 0) set_empty = 1;  // should this go to DEQRT?
                     next = IDLE;
                 end else if (wr_addr == D - 2) next = DEQRT;
                 else next = DEQ1;
@@ -165,6 +163,7 @@ module qq_fsm #(
                 we = 1;
                 deq_o = 1;
                 clr_wraddr = 1;
+                next = IDLE;
             end
             // We don't need a default here since we set default values before the case
         endcase
